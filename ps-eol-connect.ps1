ddbe23786a91ccc1a414c382046d8b48921dff7c
puchupuchu <32712975+puchupuchu@users.noreply.github.com>
2018-05-06 14:03:11 +0800

    Add files via upload

---
+++ ps-eol-connect.ps1
@@ -0,0 +1,15 @@
+﻿Param (
+    [Parameter(Mandatory=$True)]
+    [string]$username_with_domain,
+    [Parameter(Mandatory=$True)]
+    [string]$password
+    )
+
+$SecPaswd= ConvertTo-SecureString –String $($password) –AsPlainText –Force
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $($username_with_domain), $SecPaswd
+$mySession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $cred -Authentication Basic -AllowRedirection
+Import-PSSession $mySession
+
+
+
+
