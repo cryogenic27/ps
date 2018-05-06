81dc2437e8ce4daee833f9e9fd90a824ad9a79a9
puchupuchu <32712975+puchupuchu@users.noreply.github.com>
2018-05-06 20:12:07 +0800

    Add files via upload

---
+++ ps-eol-get-dyndl-members.ps1
@@ -0,0 +1,17 @@
+﻿Param (
+    [Parameter(Mandatory=$True)]
+    [string]$365DynamicDistributionGroupName
+)
+
+$dldetails = Get-DynamicDistributionGroup $365DynamicDistributionGroupName
+$members = Get-Recipient -RecipientPreviewFilter $dldetails.RecipientFilter -ResultSize unlimited | foreach {$_.Name}
+
+#OUTPUT
+echo "=========== ======== ======== ==========="
+echo "*   DYNAMIC DISTRIBUTION GROUP MEMBERS  *"
+echo ""
+echo $members
+echo ""
+echo "====== END OF OUTPUT ======"
+
+
