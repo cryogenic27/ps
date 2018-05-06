1420f06e26f53eb01a2f511f8a785cd7b799e621
puchupuchu <32712975+puchupuchu@users.noreply.github.com>
2018-05-06 19:54:08 +0800

    Add files via upload

---
+++ ps-eol-maillog-subject.ps1
@@ -0,0 +1,41 @@
+﻿
+Param (
+    [Parameter(Mandatory=$True)]
+    [string]$SenderAddress,
+    [Parameter(Mandatory=$True)]
+    [string]$Subject,
+    [Parameter(Mandatory=$True)]
+    [string]$hours_to_track_back
+
+    )
+
+
+$dateEnd = get-date 
+$dateStart = $dateEnd.AddHours($("-" + $hours_to_track_back))
+ 
+    $message = Get-MessageTrace -StartDate $dateStart -EndDate $dateEnd -SenderAddress $SenderAddress -Pagesize 5000 -Page 1 `
+    | Select-Object Received,SenderAddress,RecipientAddress,Subject,Status
+
+
+echo ""
+echo ""
+echo "===== ===== ===== ====== ====="
+echo "Basis: RECIPIENT -> $SenderAddress"
+echo "Back tracking $hours_to_track_back hour(s) from present."
+echo "... ..."
+echo "Begin Mail Delivery Log"
+echo "... ..."
+echo ""
+echo "Status, Received,RecipientAddress,Subject"
+foreach($x in $message)
+{
+    if($($x.Subject) -like "*$Subject*"){
+        $($($x.Status)+"`t"+$($x.Received)+"`t"+$($x.RecipientAddress)+"`t"+$($x.Subject))
+    }
+
+}
+
+echo ""
+echo "===== ===== END OF OUTPUT ====== ====="
+
+
