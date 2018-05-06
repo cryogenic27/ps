ea4689831954f5c5e164274d16f22cf2087c3cd6
puchupuchu <32712975+puchupuchu@users.noreply.github.com>
2018-05-06 15:55:54 +0800

    Add files via upload

---
+++ ps-eol-get-contact.ps1
@@ -0,0 +1,18 @@
+﻿Param (
+    [Parameter(Mandatory=$True)]
+    [string]$365Contact
+)
+
+$details = Get-Contact -identity $365Contact `
+|Select-Object DisplayName,RecipientType,WindowsEmailAddress,WhenCreated
+
+#OUTPUT
+echo "====== ===== ===== ======"
+echo "*   CONTACT PROPERTIES  *"
+echo ""
+echo "Display Name: $($details.DisplayName)"
+echo "Email Addresses: $($details.WindowsEmailAddress)"
+echo "RecipientType: $($details.RecipientType)"
+echo "Creation Date: $($details.WhenCreated)"
+echo ""
+echo "====== END OF OUTPUT ======"
