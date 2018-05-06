fbb54aec28b3c6f22a0c1733ee22ceb06dcd3337
puchupuchu <32712975+puchupuchu@users.noreply.github.com>
2018-05-06 21:10:05 +0800

    Add files via upload

---
+++ ps-eol-get-sharedmailbox-delegated.ps1
@@ -0,0 +1,19 @@
+﻿
+Param (
+    [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+    [AllowEmptyString()]
+    [string]$sharedMailbox
+    )
+
+$result2 = @()
+$result1 = Get-MailboxPermission -Identity $shared | ft identity,user,accessrights | Out-String
+$result2 = $result1.Split([Environment]::NewLine)
+
+Echo "============= DELEGATED USERS ============="
+foreach($x in $result2){
+    if($x -like "*@*"){
+    echo $x
+    }
+}
+Echo "============= END OF RESULT ============="
+
