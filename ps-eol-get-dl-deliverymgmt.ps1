545a58aca4d2775c288d318fb647640db569601b
puchupuchu <32712975+puchupuchu@users.noreply.github.com>
2018-05-06 19:13:17 +0800

    Add files via upload

---
+++ ps-eol-get-dl-deliverymgmt.ps1
@@ -0,0 +1,19 @@
+﻿Param (
+    [Parameter(Mandatory=$True)]
+    [string]$365DistributionGroup
+)
+
+$details = Get-DistributionGroup -identity $365DistributionGroup | select -ExpandProperty AcceptMessagesOnlyFromSendersOrMembers
+
+#OUTPUT
+echo "=========== ======== ======== ==========="
+echo "* DISTRIBUTION GROUP PERMITTED SENDERS *"
+echo ""
+
+echo "Group Name / Alias: $365DistributionGroup"
+echo ""
+echo $details
+echo ""
+echo "====== END OF OUTPUT ======"
+
+
