df44de525efccdd14b5a6fba1aa0a73f8a3bb4da
puchupuchu <32712975+puchupuchu@users.noreply.github.com>
2018-05-08 13:33:45 +0800

    Add files via upload

---
+++ ps-eol-get-sharedmailbox-delegate_sentitems.ps1
@@ -0,0 +1,21 @@
+﻿Param (
+    [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+    [AllowEmptyString()]
+    [string]$sharedmailbox_with_domain
+    )
+
+
+set-mailbox $sharedmailbox_with_domain -MessageCopyForSentAsEnabled $True -Force
+set-mailbox $sharedmailbox_with_domain -MessageCopyForSendOnBehalfEnabled $True -Force
+
+$result = Get-Mailbox $sharedmailbox_with_domain |select-object Alias,PrimarySMTPAddress,MessageCopyForSentAsEnabled,MessageCopyForSendOnBehalfEnabled
+
+echo "==================================================="
+echo ""
+echo "Getting Sent Items Config for $sharedmailbox_with_domain"
+echo "Alias: $($result.Alias)"
+echo "Email Address: $($result.PrimarySMTPAddress)"
+echo "Allow Send As delegates to send emails to their sent items: $($result.MessageCopyForSentAsEnabled)"
+echo "Allow Send On Behalf delegates to send emails to their sent items: $($result.MessageCopyForSendOnBehalfEnabled)"
+echo ""
+echo "==================================================="
